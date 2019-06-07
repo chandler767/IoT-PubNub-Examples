8c8a82678548686e85705e012bcd324bd5bdf744
Chandler Mayo <chandler@chandlermayo.com>
2019-06-07 14:28:15 -0700

    Cleaning up comments.

--- ATWINC1500/SDK/SDK.ino
+++ ATWINC1500/SDK/SDK.ino
@@ -2,44 +2,36 @@
   PubNub example using ATWINC1500.
 */
 
-
 #include <WiFi101.h>
 #define PubNub_BASE_CLIENT WiFiClient
 #include <PubNub.h>
 
-static char ssid[] = "pubnub-visitor"; // SSID
-static char pass[] = "data-stream!"; // Password
+static char ssid[] = "YOUR_NETWORK_SSID";
+static char pass[] = "YOUR_NETWORK_PASSWORD";
 
-const static char pubkey[]  = "pub-c-5e98b32b-48a6-4616-881f-4e3770ed58eb";
-const static char subkey[]  = "sub-c-0a38b39c-7373-11e9-912a-e2e853b4b660";
+const static char pubkey[]  = "YOUR_PUB_KEY_HERE";
+const static char subkey[]  = "YOUR_SUB_KEY_HERE";
 const static char channel[] = "hello_world";
 
 String message;
 
-void setup()
-{
-    Serial.begin(9600);
-
-    // Connect to WiFi
+void setup() {
+    Serial.begin(9600); 
     Serial.println("Attempting to connect...");
-    if(WiFi.begin(ssid, pass) != WL_CONNECTED) {
+    if(WiFi.begin(ssid, pass) != WL_CONNECTED) { // Connect to WiFi.
         Serial.println("Couldn't connect to WiFi.");
         while(1) delay(100);
     }
     else {
         Serial.print("Connected to SSID: ");
         Serial.println(ssid);
-        PubNub.begin(pubkey, subkey); // When connected start PubNub.
+        PubNub.begin(pubkey, subkey); // Start PubNub.
         Serial.println("PubNub is set up.");
     }
 }
 
-
-void loop()
-{
-
-    /* Subscribe */ 
-    {
+void loop() {
+    { // Subscribe
         PubSubClient* sclient = PubNub.subscribe(channel); // Subscribe for messages.
         if (0 == sclient) { 
             Serial.println("Error subscribing to channel.");
@@ -51,10 +43,8 @@ void loop()
         }
         sclient->stop();
     }
-
-   /* Publish */
-   {
-         char msg[] = "\"Hello world\"";
+    { // Publish
+        char msg[] = "\"Hello world\"";
         WiFiClient* client = PubNub.publish(channel, msg); // Publish message to channel.
         if (0 == client) {
             Serial.println("Error publishing message.");
@@ -63,6 +53,5 @@ void loop()
         }
         client->stop();
     }
-
     delay(1000);
 }
